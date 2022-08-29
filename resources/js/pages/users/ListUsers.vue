4f6bdb5578851d912024e5a4d3e0f30c97f608e5
Veasna <Veasna3d@gmail.com>
2022-08-29 22:18:27 +0700

    validate user

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,8 +1,11 @@
 <script setup>
 import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, reactive } from 'vue';
+import { Form, Field } from 'vee-validate';
+import * as yup from 'yup';
 
 const users = ref([]);
+
 
 const getUsers = () => {
     axios.get('/api/users')
@@ -10,6 +13,35 @@ const getUsers = () => {
             users.value = response.data;
         })
 }
+
+const schema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().min(8),
+
+});
+
+const createUser = (values, { resetForm }) =>{
+    axios.post('/api/users', values)
+    .then((response) =>{
+        users.value.unshift(response.data);
+        $('#createUserModal').modal('hide');
+        resetForm();
+    })
+};
+
+// const createUser = () => {
+//     axios.post('/api/users', form)
+//         .then((response) => {
+
+//             users.value.unshift(response.data);
+
+//             form.name = '',
+//                 form.email = '',
+//                 form.password = '';
+//             $('#createUserModal').modal('hide');
+//         })
+// }
 
 
 onMounted(() => {
@@ -35,6 +67,10 @@ onMounted(() => {
     </div>
     <div class="content">
         <div class="container-fluid">
+            <!-- Button trigger modal -->
+            <button type="button" class="mb-2 btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+                Add New User
+            </button>
             <div class="card">
                 <div class="card-body">
                     <table class="table table-bordered">
@@ -60,6 +96,49 @@ onMounted(() => {
                         </tbody>
                     </table>
                 </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="createUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createUserModalLabel">Add New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <From @submit="createUser" :validation-schema="schema" v-slot="{ errors }" >
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <Field name="name" type="text" class="form-control" :class="{'is-invalid': errors.name }" id="name"
+                                aria-describedby="nameHelp" placeholder="Enter full name"/>
+                                <span class="invalid-feedback">{{ errors.name }}</span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <Field name="email" type="email" class="form-control " :class="{'is-invalid': errors.email }"  id="email"
+                                aria-describedby="nameHelp" placeholder="Enter full name"/>
+                                <span class="invalid-feedback">{{ errors.email }}</span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Password</label>
+                            <Field name="password" type="password" class="form-control " :class="{'is-invalid': errors.password }"  id="password"
+                                aria-describedby="nameHelp" placeholder="Enter password"/>
+                                <span class="invalid-feedback">{{ errors.password }}</span>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </From>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,7 @@ Route::get('/', function () {
 //     return view('dashboard');
 // });
 Route::get('/api/users', [UserController::class, 'index']);
+Route::post('/api/users', [UserController::class , 'store']);
+
+
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
